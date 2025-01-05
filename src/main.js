93fc8c0a9b1b0becc8d9a989d2c5f73b3d8e4d0e
Samuel Colin <samuel@macbook-air-de-samuel.home>
2025-01-05 11:53:35 +0100

    Initialisation du nouveau template de site V2

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+/**
+ * Created by Ryan Balieiro on 08.23.2023
+ * App's entry point
+ */
+import './scss/style.scss'
+import {useData} from "./composables/data.js"
+import {useLanguage} from "./composables/language.js"
+import {useNavigation} from "./composables/navigation.js"
+import {createAppRouter} from "./router/router.js"
+import {createApp} from "vue"
+import App from './vue/core/App.vue'
 
-// Materialize
-import 'materialize-css/dist/css/materialize.min.css'
-import 'material-design-icons/iconfont/material-icons.css'
+const data = useData()
 
-createApp(App).mount('#app')
+data.fetchEssentials().then(r => {
+    const language = useLanguage()
+    language.init(data.getSettings()['supportedLanguages'])
+
+    const navigation = useNavigation()
+    navigation.init(data.getSections(), data.getCategories())
+
+    const router = createAppRouter()
+    createApp(App).use(router).mount('#app')
+})
